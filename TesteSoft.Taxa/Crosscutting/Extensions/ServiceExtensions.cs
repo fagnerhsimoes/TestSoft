cf573b20de481562ae520dfc5ca3a60787bc790c
FHSIMOES <FHSIMOES@PD-31>
2021-06-27 14:39:46 -0300

    Ajuste Config- Taxa API

--- TesteSoft.Taxa/Crosscutting/Extensions/ServiceExtensions.cs
+++ TesteSoft.Taxa/Crosscutting/Extensions/ServiceExtensions.cs
@@ -36,7 +36,7 @@ namespace TesteSoft.Taxa.Crosscutting.Extensions
                     {
                         Name = "Fagner Simões",
                         Email = "fagnerhsimoes@gmail.com",
-                        Url = new Uri("https://github.com/fagnerhsimoes/ISys")
+                        Url = new Uri("https://github.com/fagnerhsimoes/TestSoft")
                     }
                 });
                 c.AddSecurityDefinition("API Key", new OpenApiSecurityScheme
