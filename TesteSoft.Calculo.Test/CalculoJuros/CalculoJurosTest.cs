77f1048760fc5455e9c783c7ffe021ee5ceef63e
FHSIMOES <FHSIMOES@PD-31>
2021-06-27 17:39:00 -0300

    Adiciona Teste Unitário CalculoJuros

---
+++ TesteSoft.Calculo.Test/CalculoJuros/CalculoJurosTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TesteSoft.Calculo.Test.CalculoJuros
+{
+
+    [TestClass]
+    public class CalculoJurosTest
+    {
+        [TestMethod]
+        public void RetornaErroQuandoResultadoCalculoInvalido()
+        {
+            string resultadoTeste = "105,09";
+            var ResultadoCalculo = "105,10";
+
+            bool ResultadoValido = ResultadoCalculo == resultadoTeste;
+            Assert.IsTrue(!ResultadoValido);
+        }
+
+        [TestMethod]
+        public void RetornaSucessoQuandoResultadoCalculoValido()
+        {
+            string resultadoTeste = "105,10";
+            var ResultadoCalculo = "105,10";
+
+            bool ResultadoValido = ResultadoCalculo == resultadoTeste;
+            Assert.IsTrue(ResultadoValido);
+        }
+    }
+}
